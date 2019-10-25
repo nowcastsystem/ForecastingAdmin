1548bf28c5aa8b229d3bddd1b84ac544ccfa7996
Terry Tan <tmy1995hflc@gmail.com>
2019-10-25 16:30:54 -0500

    change task list

--- src/main.js
+++ src/main.js
@@ -1,25 +1,28 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
-import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import 'normalize.css/normalize.css'; // a modern alternative to CSS resets
 
-import Element from 'element-ui'
-import './styles/element-variables.scss'
+import Element from 'element-ui';
+import './styles/element-variables.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import '@/styles/index.scss' // global css
+import '@/styles/index.scss'; // global css
 
-import App from './App'
-import store from './store'
-import router from './router'
+import App from './App';
+import store from './store';
+import router from './router';
 
-import './icons' // icon
-import './permission' // permission control
-import './utils/error-log' // error log
+import './icons'; // icon
+import './permission'; // permission control
+import './utils/error-log'; // error log
 
-import * as filters from './filters' // global filters
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import * as filters from './filters'; // global filters
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /**
  * If you don't want to use mock-server
@@ -29,37 +32,41 @@ import 'bootstrap/dist/css/bootstrap.min.css'
  * Currently MockJs will be used in the production environment,
  * please remove it before going online! ! !
  */
-import { mockXHR } from '../mock'
+import { mockXHR } from '../mock';
 if (process.env.NODE_ENV === 'production') {
-  mockXHR()
+  mockXHR();
 }
+
+library.add(faUserSecret);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
-})
+});
 
 // register global utility filters
 Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-  Vue.filter('formatSize', function (size) {
-  if (size > 1024 * 1024 * 1024 * 1024) {
-    return (size / 1024 / 1024 / 1024 / 1024).toFixed(2) + ' TB'
-  } else if (size > 1024 * 1024 * 1024) {
-    return (size / 1024 / 1024 / 1024).toFixed(2) + ' GB'
-  } else if (size > 1024 * 1024) {
-    return (size / 1024 / 1024).toFixed(2) + ' MB'
-  } else if (size > 1024) {
-    return (size / 1024).toFixed(2) + ' KB'
-  }
-  return size.toString() + ' B'
-})
-})
+  Vue.filter(key, filters[key]);
+  Vue.filter('formatSize', function(size) {
+    if (size > 1024 * 1024 * 1024 * 1024) {
+      return (size / 1024 / 1024 / 1024 / 1024).toFixed(2) + ' TB';
+    } else if (size > 1024 * 1024 * 1024) {
+      return (size / 1024 / 1024 / 1024).toFixed(2) + ' GB';
+    } else if (size > 1024 * 1024) {
+      return (size / 1024 / 1024).toFixed(2) + ' MB';
+    } else if (size > 1024) {
+      return (size / 1024).toFixed(2) + ' KB';
+    }
+    return size.toString() + ' B';
+  });
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
   router,
   store,
   render: h => h(App)
-})
+});
